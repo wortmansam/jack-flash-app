4bf50927391d1301157b7dd15b2804f93070edcc
Sam Wortman <swortman@wortmanstores.com>
2025-11-21 16:21:17 -0600

    Update Bundle ID to match Apple Developer

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.wortmanstores',
+  appId: 'com.wortmanstores.jackflash',
   appName: 'Jack Flash',
   webDir: 'build'
 };
